56b73a224ff13f29c3ee550846dd607414a7db97
delwar20h <delwar20h@yahoo.com>
2017-07-27 14:14:13 -0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+// My progress with Object Oriented Programming halted here. My understanding of it is no where
+// near where it should be. I have much work to do to fully grasp it. With OPP there are so many new
+// principals introduced including, getters, setters, toStrings, constructors and various class types I just
+// can't seem to put it all together.
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = scan.nextLine();
+
+        System.out.println("Would you like to play against TheJets or TheSharks (j/s)?:");
+
+    }
+}
---
+++ src/Roshambo.java
@@ -0,0 +1,17 @@
+public enum Roshambo {
+
+    ROCK, PAPER, SCISSORS;
+
+    public String toString() {
+        switch (this) {
+            case ROCK:
+                return "Rock!";
+            case PAPER:
+                return "Paper!";
+            default:
+                return "Scissors!";
+
+        }
+    }
+}
+
